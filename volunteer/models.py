1c9b1f35836c8e1cc4e955b1d757362785778ad3
esmaeil kazemi <smk182018@gmail.com>
2024-03-14 08:24:34 +0330

    Feat(): add verbose name and change forignkey

--- volunteer/models.py
+++ volunteer/models.py
@@ -1,25 +1,12 @@
 from django.db import models
-
-
-class Status(models.Model):
-    name = models.CharField(max_length=200)
-    model = models.CharField(max_length=50)
-    is_active = models.BooleanField(default=True)
-    color = models.CharField(blank=True, null=True, max_length=200)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "وضعیت "
-        verbose_name_plural = "وضعیت ها"
+from django.utils.translation import gettext as _
 
 
 class State(models.Model):
-    name = models.CharField(max_length=200)
-    title = models.CharField(max_length=300, blank=True, null=True)
-    color = models.CharField(blank=True, null=True, max_length=200)
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=200, verbose_name=_("Name"))
+    title = models.CharField(max_length=300, blank=True, null=True, verbose_name=_("Title"))
+    color = models.CharField(blank=True, null=True, max_length=200, verbose_name=_("Color"))
+    is_active = models.BooleanField(default=True, verbose_name=_("Is active"))
 
     class Meta:
         verbose_name = "موقعیت "
@@ -29,18 +16,36 @@ class State(models.Model):
         return self.title
 
 
+class Status(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_("Name"))
+    key = models.CharField(max_length=200, verbose_name=_("key"))
+    is_active = models.BooleanField(default=True, verbose_name=_("Is active"))
+    color = models.CharField(blank=True, null=True, max_length=200, verbose_name=_("Color"))
+    order = models.PositiveIntegerField(default=1, verbose_name=_("Order"))
+    state = models.ForeignKey(State, on_delete=models.CASCADE, verbose_name=_("State"))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "وضعیت "
+        verbose_name_plural = "وضعیت ها"
+
+
 class Volunteer(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=300)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=11)
-    year_of_birth = models.PositiveIntegerField()
-    status = models.ForeignKey(Status, on_delete=models.DO_NOTHING, related_name="volunteers",
-                               limit_choices_to={'model': "Volunteer", "is_active": True})
-    state = models.ForeignKey(State, on_delete=models.DO_NOTHING, related_name="volunteers")
-    url_github = models.URLField(blank=True, null=True)
-    url_linkedin = models.URLField(unique=True)
-    register_time = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=200, verbose_name=_("First name"))
+    last_name = models.CharField(max_length=300, verbose_name=_("Last name"))
+    email = models.EmailField(unique=True, verbose_name=_("Email"))
+    phone_number = models.CharField(max_length=11, verbose_name=_("Phone"))
+    year_of_birth = models.PositiveIntegerField(verbose_name=(_("Year of birth")))
+    state = models.ForeignKey(State, on_delete=models.DO_NOTHING, limit_choices_to={"is_active": True},
+                              related_name="volunteers")
+    status = models.ForeignKey(Status, on_delete=models.DO_NOTHING,
+                               limit_choices_to={"is_active": True})
+    url_github = models.URLField(blank=True, null=True, verbose_name=_("Url github"))
+    url_linkedin = models.URLField(verbose_name=_("Url linkedin"))
+    register_time = models.DateTimeField(auto_now_add=True, verbose_name=_("Register time"))
+    is_send_email = models.BooleanField(default=False, verbose_name=_("Is send email"))
 
     def __str__(self):
         return f"{self.first_name}-{self.last_name}"
