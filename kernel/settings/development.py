983aa0b78bbb89bbc17d6cddd2334ed80d2abbd3
esmaeil kazemi <smk182018@gmail.com>
2024-07-21 20:31:06 +0330

    Feat(): change database

--- kernel/settings/development.py
+++ kernel/settings/development.py
@@ -7,8 +7,12 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.spl
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USERNAME"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
