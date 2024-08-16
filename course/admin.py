cc842144b1a3b7cf83a41323f080a624cc587d3d
esmaeil kazemi <smk182018@gmail.com>
2024-08-16 12:04:34 +0330

    Style(): update

--- course/admin.py
+++ course/admin.py
@@ -5,6 +5,8 @@ from course.models import Course
 class CourseAdmin(admin.ModelAdmin):
     list_display = (
         'name',
+        'start_date',
+        'end_date',
         "is_active",
     )
     list_filter = ("is_active",)
