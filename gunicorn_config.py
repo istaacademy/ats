6365c2d1aa70401ae4a9d4057bc6bbb64a7b170d
esmaeilkazemi <esmaeilkazemi@hamravesh.com>
2025-05-09 00:24:21 +0330

    Fix: fix address

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,5 +7,5 @@ workers = 4  # Adjust based on your server's resources
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/www.istaacademy.com/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/www.istaacademy.com/privkey.pem"
+certfile = "/etc/letsencrypt/live/istaacademy.com/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/istaacademy.com/privkey.pem"
